74baa2d3266c7535777f035d952b5abb55f337df
EstebanProg <ecortescast2345@gmail.com>
2025-04-17 11:29:18 -0500

    2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
         <Routes>
-           <Route path='/' element={<FormExample/>} />
+           <Route path='/principal' element={<FormExample/>} />
         </Routes>
     </BrowserRouter>
   )
